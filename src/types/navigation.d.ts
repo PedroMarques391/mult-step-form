87a5638225c1662317a10ceac103a40ddd92bab0
Pedro Marques <pedromarques391@gmail.com>
2025-07-11 18:15:30 -0300

    types: create a declaration types to React Navigation.

---
+++ src/types/navigation.d.ts
@@ -0,0 +1,9 @@
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            stepOne: undefined
+            stepTwo: undefined
+            stepThree: undefined
+        }
+    }
+}
